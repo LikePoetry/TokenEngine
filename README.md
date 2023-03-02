64300676f22b37fb45a00eb4de5784faa6ddb0b3
LikePoetry <38221738+LikePoetry@users.noreply.github.com>
2023-03-02 18:11:52 +0800

    开发环境搭建测试验证！

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # TokenEngine
 Token of allegiance for vulkan
+
+## 第三方开源库支持
+vendor 文件夹下内容
+++++ GLFW               窗口支持
+++++ glm                数学计算库
+++++ imgui              UI支持
+++++ spdlog             日志支撑
+++++ stb                图片导入处理
+++++ tinyobjloader      3D模型导入
+
+
+## 编译流程
+环境：
+1、Windows 系统
+2、安装Vulkan SDK
+3、VS2022 或  VS2019
+
+步骤：
+1、双击 Run_VS2022.bat；
+2、打开 TokenEngine.sln;
+
+若安装的是VS2019 修改 Run_VS2022.bat文件，
+替换<call Tools\premake5.exe vs2022> 为 <call Tools\premake5.exe vs2019>
--- Sandbox/main.cpp
+++ Sandbox/main.cpp
@@ -1,10 +1,45 @@
 // vulkan 学习进程文件，每次提交保证项目能够顺利运行;
 
-//2023-3-2 11:30:48 项目结构文件;
+//2023年3月2日18:05:29 开发环境搭建测试验证;
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
 
 #include <iostream>
 
 int main()
 {
-	std::cout << "Hello world!" << std::endl;
+	//初始化 GLFW环境
+	glfwInit();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+
+    //创建 Vulkan 窗口
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+
+    uint32_t extensionCount = 0;
+
+    // 查询扩展信息;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+    std::cout << extensionCount << " extensions supported\n";
+
+    glm::mat4 matrix;
+    glm::vec4 vec;
+    auto test = matrix * vec;
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+
+    return 0;
 }
