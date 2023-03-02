fd56eb8f606a914a39e208b2ac5226d200fe4a91
LikePoetry <38221738+LikePoetry@users.noreply.github.com>
2023-03-02 11:37:27 +0800

    项目初始化，相关vendor 见外部

---
+++ Run_VS2022.bat
@@ -0,0 +1,3 @@
+call Tools\premake5.exe vs2022
+
+pause
---
+++ Sandbox/main.cpp
@@ -0,0 +1,10 @@
+// vulkan 学习进程文件，每次提交保证项目能够顺利运行;
+
+//2023-3-2 11:30:48 项目结构文件;
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello world!" << std::endl;
+}
